d0b71d7caa3b244cb23bab06ae0ce9827bf5e813
Hossain Md Athar <hossain45@Hossains-MacBook-Air.local>
2025-11-24 09:57:45 +0600

    feat: add favicon

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import "./index.css";
 import App from "./routes/App.tsx";
 import store from "./store/store.ts";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
